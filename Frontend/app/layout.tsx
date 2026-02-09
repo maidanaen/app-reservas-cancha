51a108353ab0a1bbc34ba918f8bc74aa13bcc4cf
maidaneen <maidanenzosebastian@gmail.com>
2026-02-09 20:11:22 -0300

    cambiando el modo noche

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -1,19 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 // 👇 CAMBIO 1: Importamos el Condicional en lugar del Navbar directo
 import ConditionalNavbar from "@/components/ConditionalNavbar"; 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nexus Sport Complex", // Aproveché para ponerle un título mejor
@@ -28,8 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        suppressHydrationWarning={true}
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+       className={inter.className} style={{ backgroundColor: 'white' }}
       >
         <Providers>
         {/* 👇 CAMBIO 2: Usamos el componente inteligente */}
